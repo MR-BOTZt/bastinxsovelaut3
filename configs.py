c3f3ade1c4634a1576921ef858d88450e254d572
MR-BOTZt <120255966+MR-BOTZt@users.noreply.github.com>
2023-01-14 16:24:56 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -6,7 +6,7 @@ class Config:
     BOT_TOKEN = getenv("BOT_TOKEN", "1234567891:AdDfgFRFVVfDEhdhyjjvjjftSEW")
     FSUB = getenv("FSUB", "SDBotz")
     CHID = int(getenv("CHID", "-1000112234"))
-    SUDO = list(map(int, getenv("SUDO").split()))
+    SUDO = list(map(int, getenv("SUDO", "").split()))
     MONGO_URI = getenv("MONGO_URI", "")
     
 cfg = Config()
