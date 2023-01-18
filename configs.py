6e543a16ceeeea3a8152043075a2aa726f0b2835
MR-BOTZt <120255966+MR-BOTZt@users.noreply.github.com>
2023-01-18 23:15:23 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -3,7 +3,7 @@ from os import path, getenv
 class Config:
     API_ID = int(getenv("API_ID", "22359038"))
     API_HASH = getenv("API_HASH", "b3901895dc193c30c808ba4f1b550ed0")
-    BOT_TOKEN = getenv("BOT_TOKEN", "5974710703:AAGdG-BNM-t2R853v0mWRBAdMYwWr1hZCFI")
+    BOT_TOKEN = getenv("BOT_TOKEN", "5918634201:AAGDrFfjpSXsgvKflyz5QFwiz4-MNrlEhbI")
     FSUB = getenv("FSUB", "REQUSET_ACCEPT_BOT")
     CHID = int(getenv("CHID", "-1001745001470"))
     SUDO = list(map(int, getenv("SUDO", "5531461861 1878911986").split()))
