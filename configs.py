21e6733874c9a13b11cbaef105fbc689698825d6
MR-BOTZt <120255966+MR-BOTZt@users.noreply.github.com>
2023-01-21 01:00:07 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -3,10 +3,10 @@ from os import path, getenv
 class Config:
     API_ID = int(getenv("API_ID", "22359038"))
     API_HASH = getenv("API_HASH", "b3901895dc193c30c808ba4f1b550ed0")
-    BOT_TOKEN = getenv("BOT_TOKEN", "5918634201:AAGDrFfjpSXsgvKflyz5QFwiz4-MNrlEhbI")
+    BOT_TOKEN = getenv("BOT_TOKEN", "5913406486:AAHIZH8MQE0-8Qct8QRv6IbjfSK3OeddOsE")
     FSUB = getenv("FSUB", "REQUSET_ACCEPT_BOT")
     CHID = int(getenv("CHID", "-1001745001470"))
     SUDO = list(map(int, getenv("SUDO", "5531461861 1878911986").split()))
-    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://evatgbottelegram:evatgbottelegram@cluster0.hckeuhb.mongodb.net/?retryWrites=true&w=majority")
+    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://bastin:bastin@cluster0.j4oqpkq.mongodb.net/?retryWrites=true&w=majority")
     
 cfg = Config()
